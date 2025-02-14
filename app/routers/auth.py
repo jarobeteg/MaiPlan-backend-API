ba6601fa469103c0d6452b2dfe8fb6f68ae3816b
jarobeteg <jaroslavtm@gmail.com>
2025-02-14 11:39:51 +0100

    code refactoring

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,163 +11,81 @@ router = APIRouter()
 
 EMAIL_REGEX = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 
+def validate_email(email: str):
+    email = email.strip()
+    if not email:
+        raise HTTPException(status_code=400, detail={"code": 1, "message": "Email field cannot be empty!"})
+    if not re.match(EMAIL_REGEX, email):
+        raise HTTPException(status_code=400, detail={"code": 2, "message": "Invalid email format!"})
+    return email
+
+async def validate_email_existence(db: AsyncSession, email: str):
+    existing_user = await get_user_by_email(db, email)
+    if not existing_user:
+        raise HTTPException(status_code=401, detail={"code": 3, "message": "Email is not yet registered!"})
+    return existing_user
+
+def validate_password(password: str):
+    password = password.strip()
+    if not password:
+        raise HTTPException(status_code=400, detail={"code": 4, "message": "Password field cannot be empty!"})
+    return password
+
+def validate_password_strength(password: str):
+    if not is_valid_password(password):
+        raise HTTPException(status_code=400, detail={"code": 5, "message": "Password is not strong enough!"})
+
+def validate_passwords(password: str, password_again: str):
+    if not password_again:
+        raise HTTPException(status_code=400, detail={"code": 6, "message": "Password again field cannot be empty!"})
+    if not do_passwords_match(password, password_again):
+        raise HTTPException(status_code=400, detail={"code": 7, "message": "Passwords do not match!"})
+
+def get_access_token(user_id: str):
+    token_data = {"sub": user_id}
+    return create_access_token(data=token_data)
+
 @router.post("/register", response_model=Token)
 async def register(user: UserRegister, db: AsyncSession = Depends(get_db)):
-    if not user.email.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 1, "message": "Email field cannot be empty!"}
-                )
+    user.email = validate_email(user.email)
+    user.username = user.username.strip()
 
-    if not re.match(EMAIL_REGEX, user.email.strip()):
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 2, "message": "Invalid email format!"}
-                )
+    if await get_user_by_email(db, user.email):
+        raise HTTPException(status_code=400, detail={"code": 8, "message": "Email is already taken!"})
+    if not user.username:
+        raise HTTPException(status_code=400, detail={"code": 9, "message": "Username field cannot be empty!"})
+    if await get_user_by_username(db, user.username):
+        raise HTTPException(status_code=400, detail={"code": 10, "message": "Username is already taken!"})
 
-    taken_email = await get_user_by_email(db, user.email.strip())
-    
-    if taken_email:
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 3, "message": "Email is already taken!"}
-                )
-
-    if not user.username.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 4, "message": "Username field cannot be empty!"}
-                )
-
-    taken_username = await get_user_by_username(db, user.username.strip())
-
-    if taken_username:
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 5, "message": "Username is already taken!"}
-                )
-
-    if not user.password.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 6, "message": "Password field cannot be empty!"}
-                )
-
-    if not is_valid_password(user.password.strip()):
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 7, "message": "Password is not strong enough!"}
-                )
-
-    if not user.password_again.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 8, "message": "Password again field cannot be empty!"}
-                )
-
-    if not do_passwords_match(user.password.strip(), user.password_again.strip()):
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 9, "message": "Passwords do not match!"}
-                )
-
+    user.password = validate_password(user.password)
+    validate_password_strength(user.password)
+    validate_passwords(user.password, user.password_again.strip())
     new_user = await create_user(db, user)
-    existing_email = await get_user_by_email(db, new_user.email.strip())
-    
-    token_data = {"sub": existing_email.user_id}
-    access_token = create_access_token(data=token_data)
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return Token(access_token=get_access_token(new_user.user_id), token_type="bearer")
 
 @router.post("/login", response_model=Token)
 async def login(user: UserLogin, db: AsyncSession = Depends(get_db)):
-    if not user.email.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 1, "message": "Email field cannot be empty!"}
-                )
+    user.email = validate_email(user.email)
+    existing_user = await validate_email_existence(db, user.email)
 
-    if not re.match(EMAIL_REGEX, user.email.strip()):
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 2, "message": "Invalid email format!"}
-                )
+    user.password = validate_password(user.password)
+    if not verify_password(user.password, existing_user.password_hash):
+        raise HTTPException(status_code=401, detail={"code": 8, "message": "Incorrect password!"})
 
-    if not user.password.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 3, "message": "Password field cannot be empty!"}
-                )
-
-    existing_email = await get_user_by_email(db, user.email.strip())
-    if not existing_email:
-        raise HTTPException(
-                status_code=401,
-                detail={"code": 4, "message": "Email is not yet registered!"}
-                )
-
-    if not verify_password(user.password.strip(), existing_email.password_hash):
-        raise HTTPException(
-                status_code=401,
-                detail={"code": 5, "message": "Incorrect password!"}
-                )
-    
-    token_data = {"sub": existing_email.user_id}
-    access_token = create_access_token(data=token_data)
-
-    return {"access_token": access_token, "token_type": "bearer"}
+    return Token(access_token=get_access_token(existing_user.user_id), token_type="bearer")
 
 @router.post("/reset-password", response_model=Token)
 async def reset_password(user: UserResetPassword, db: AsyncSession = Depends(get_db)):
-    if not user.email.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 1, "message": "Email field cannot be empty!"}
-                )
+    user.email = validate_email(user.email)
+    existing_user = await validate_email_existence(db, user.email)
 
-    if not re.match(EMAIL_REGEX, user.email.strip()):
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 2, "message": "Invalid email format!"}
-                )
-
-    existing_email = await get_user_by_email(db, user.email.strip())
-    if not existing_email:
-        raise HTTPException(
-                status_code=401,
-                detail={"code": 3, "message": "Email is not yet registered!"}
-                )
-
-    if not user.password.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 4, "message": "Password field cannot be empty!"}
-                )
-
-    if not is_valid_password(user.password.strip()):
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 5, "message": "Password is not strong enough!"}
-                )
-
-    if not user.password_again.strip():
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 6, "message": "Password again field cannot be empty!"}
-                )
-   
-    if not do_passwords_match(user.password.strip(), user.password_again.strip()):
-        raise HTTPException(
-                status_code=400,
-                detail={"code": 7, "message": "Passwords do not match!"}
-                )
-    
+    user.password = validate_password(user.password)
+    validate_password_strength(user.password)
+    validate_passwords(user.password, user.password_again.strip())
     await reset_user_password(db, user)
 
-    token_data = {"sub": existing_email.user_id}
-    access_token = create_access_token(data=token_data)
-
-    return {"access_token": access_token, "token_type": "bearer"}
+    return Token(access_token=get_access_token(existing_user.user_id), token_type="bearer")
 
 
 # an example of a protected route by jwt
