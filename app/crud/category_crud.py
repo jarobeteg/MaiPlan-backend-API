b8818afcac0ea8203463919955e7ce745bf0ea9f
jarobeteg <jaroslavtm@gmail.com>
2025-11-02 15:50:28 +0100

    first version of category sync coded

--- app/crud/category_crud.py
+++ app/crud/category_crud.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import expression
 from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from models import Category
-from schemas.category_schema import CategoryCreate, CategoryResponse
+from schemas.category_schema import CategoryCreate, CategoryResponse, CategorySync
 
 async def add_category(db: AsyncSession, category: CategoryCreate):
     new_category = Category(user_id=category.user_id, name=category.name, description=category.description, color=category.color, icon=category.icon)
@@ -43,13 +43,35 @@ async def remove_category(db: AsyncSession, category_id: int):
     await db.commit()
     return True
 
-async def get_pending_category(db: AsyncSession, category_id: int):
+async def make_category(db: AsyncSession, category: CategorySync):
+    new_category = Category(
+        user_id=category.user_id,
+        name=category.name,
+        description=category.description,
+        color=category.color,
+        icon=category.icon,
+        created_at=category.created_at
+    )
+    db.add(new_category)
+    await db.flush()
+    new_category.server_id = new_category.category_id
+    await db.commit()
+    await db.refresh(new_category)
+    new_category_data = CategorySync.model_validate(new_category)
+    new_category_data.category_id = category.category_id
+    return new_category_data
+
+async def get_category(db: AsyncSession, category_id: int):
+    stmt = select(Category).where(expression.column("category_id") == category_id)
+    result = await db.execute(stmt)
+    return result.scalars().first
+
+async def get_pending_categories(db: AsyncSession, user_id: int):
     stmt = select(Category).where(
-        (expression.column("category_id") == category_id) &
-        ((expression.column("sync_state") == 2) | (expression.column("sync_state") == 4))
+        (expression.column("user_id") == user_id) & (expression.column("sync_state") != 0)
     )
     result = await db.execute(stmt)
-    return result.scalars().first()
+    return result.scalars().all()
 
 async def set_category_sync_state(db: AsyncSession, category_id: int, sync_state: int):
     stmt = (
--- app/routers/categories.py
+++ app/routers/categories.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_db
 from schemas.category_schema import CategoryCreate, CategoryResponse, CategorySync
 from schemas.sync_schema import SyncRequest, SyncResponse
-from crud.category_crud import get_categories, add_category, remake_category, remove_category
+from crud.category_crud import get_categories, add_category, remake_category, remove_category, make_category, get_category, get_pending_categories, set_category_sync_state
 from models import Category
 from typing import List
 
@@ -41,7 +41,37 @@ async def category_sync(request: SyncRequest[CategorySync], db: AsyncSession = D
     acknowledged = []
     rejected = []
 
-    pass
+    change = request.changes[0] if len(request.changes) > 0 else None
+
+    if not change:
+        pending_categories: List[Category] = await get_pending_categories(db, request.user_id)
+        for pending_category in pending_categories:
+            await set_category_sync_state(db, pending_category.category_id, 0)
+            await db.refresh(pending_category)
+            acknowledged.append(pending_category)
+
+        return SyncResponse(user_id=request.user_id, acknowledged=acknowledged, rejected=rejected)
+
+    for category in request.changes:
+        if category.server_id is None:
+            if category.is_deleted == 0:
+                new_category = await make_category(db, category)
+                acknowledged.append(new_category)
+            else:
+                rejected.append(category)
+        else:
+            if category.is_deleted == 0:
+                existing_category = await get_category(db, category.server_id)
+                await set_category_sync_state(db, category.server_id, 0)
+                await db.refresh(existing_category)
+                existing_category_data = CategorySync.model_validate(existing_category)
+                existing_category_data.category_id = category.category_id
+                acknowledged.append(existing_category_data)
+            else:
+                await remove_category(db, category.server_id)
+                rejected.append(category)
+
+    return SyncResponse(user_id=request.user_id, acknowledged=acknowledged, rejected=rejected)
 
 def to_category_sync(category: Category) -> CategorySync:
     return CategorySync(
@@ -52,6 +82,8 @@ def to_category_sync(category: Category) -> CategorySync:
     description=category.description,
     color=category.color,
     icon=category.icon,
+    created_at=category.created_at,
+    updated_at=category.updated_at,
     last_modified=int(category.last_modified.timestamp() * 1000),
     sync_state=category.sync_state,
     is_deleted=category.is_deleted
