8310a5d5c14b22a8efc388eb9667b275e259e6bb
jarobeteg <jaroslavtm@gmail.com>
2026-01-18 13:07:49 +0100

    incorrect date storage fixed

--- app/crud/event_crud.py
+++ app/crud/event_crud.py
@@ -4,7 +4,7 @@ from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from models import Event
 from schemas.event_schema import EventCreate, EventSync
-from datetime import datetime, time
+from datetime import datetime, time, date, timedelta
 
 async def add_event(db: AsyncSession, event: EventCreate):
     event_date = datetime.fromtimestamp(event.date / 1000).date()
@@ -38,16 +38,16 @@ async def get_events(db: AsyncSession, user_id: int):
 
 async def make_event(db: AsyncSession, event: EventSync):
     created_at = datetime.fromtimestamp(event.created_at / 1000)
-    date = datetime.fromtimestamp(event.date / 1000).date()
-    start_time = datetime.fromtimestamp(event.start_time / 1000).time()
-    end_time = datetime.fromtimestamp(event.end_time / 1000).time()
+    date_value = date(1970,1,1) + timedelta(days=event.date)
+    start_time = (datetime.min + timedelta(microseconds=event.start_time / 1000)).time()
+    end_time = (datetime.min + timedelta(microseconds=event.end_time / 1000)).time()
     new_event = Event(
         user_id=event.user_id,
         category_id=event.category_id,
         reminder_id=event.reminder_id,
         title=event.title,
         description=event.description,
-        date=date,
+        date=date_value,
         start_time=start_time,
         end_time=end_time,
         priority=event.priority,
