e78a02e14c5dcdd22de0e5025443f2967a55a371
jarobeteg <jaroslavtm@gmail.com>
2026-02-01 15:14:50 +0100

    bcrypt updated

--- app/utils/password_utils.py
+++ app/utils/password_utils.py
@@ -1,15 +1,17 @@
-from passlib.context import CryptContext
+import bcrypt
 import re
-
-# deprecated auto means that if bcrypt gets a new update then passlib will rehash old passwords
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # unused code but I leave it here just in case
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode('utf-8')
 
 def verify_password(plain_password, hashed_password) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    plain_password_bytes = plain_password.encode('utf-8')
+    hashed_password_bytes = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_password_bytes, hashed_password_bytes)
 
 def is_valid_password(password: str) -> bool:
     if len(password) < 8:
